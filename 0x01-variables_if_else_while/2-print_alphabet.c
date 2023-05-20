c56335ff2432adf72264bd13970005c5b62127ed
Francis Kioko <francmaki14@gmail.com>
2023-05-19 18:56:45 -0700

    2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: outputs the characters
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		putchar(chr);
+	}
+	return (0);
+}
