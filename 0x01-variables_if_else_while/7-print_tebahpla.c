cb937a6e2d2e1537f3baa5a7051c85b99edde614
Francis Kioko <francmaki14@gmail.com>
2023-05-21 08:30:06 -0700

    6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: outputs the result
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'z'; chr <= 'a'; chr++)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+	return (0);
+}
