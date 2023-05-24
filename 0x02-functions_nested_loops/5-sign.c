b41718585426d1dfbebe0d7bb9b0e4f595e83f88
Francis Kioko <francmaki14@gmail.com>
2023-05-24 14:46:42 -0700

    5-sign.c file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - makes the sign
+ * @n: value to be checked
+ *
+ * Return: some results
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
