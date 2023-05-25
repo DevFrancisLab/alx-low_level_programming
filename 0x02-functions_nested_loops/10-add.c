397a5c1dc3ba30744b1d0579997956564055f007
Francis Kioko <francmaki14@gmail.com>
2023-05-24 19:20:28 -0700

    10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @x: takes first value
+ * @y: takes second value
+ *
+ * Return: outputs the result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
