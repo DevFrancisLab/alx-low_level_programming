bce7651765fe6359b584d0f3bafd1b9cdbf3f938
DevFrancisLab <francmaki14@gmail.com>
2024-08-02 04:10:31 +0300

    Print Fizz Buzz game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Calculate and print Fizz Buzz
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			char str[] = "FizzBuzz";
+
+			printf("%s", str);
+			printf(" ");
+		}
+		else if (i % 3 == 0)
+		{
+			char str[] = "Fizz";
+
+			printf("%s", str);
+			printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
+			char str[] = "Buzz";
+
+			printf("%s", str);
+			printf(" ");
+		} else
+		{
+
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
