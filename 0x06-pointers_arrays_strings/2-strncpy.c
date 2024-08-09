6ea4fed154a762b0de7dd1c357b1285d176274d7
DevFrancisLab <francmaki14@gmail.com>
2024-08-10 01:02:49 +0300

    Create _strncpy function

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,33 +1,32 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies at most an input number
- * of bytes from string src into dest
+ * _strncpy - Copies at most an inputted number
+ * of bytes from string src into dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
- * @n: The maximum number of bytes to be copied
- * from src
+ * @n: The maximum number of bytes to be copied form src.
  *
- * Return: A pointer to the resulting string dest;
+ * Return: A pointer to the resulting string dest.
+ *
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int k;
 
-	while (src[index++])
+	k = 0;
+	while (k < n && src[k] != '\0')
 	{
-		src_len++;
-	}
-
-	for (index = 0; src[index] && index < n; index++)
-	{
-		dest[index] = src[index];
-	}
-
-	for (index = src_len; index < n; index++)
-	{
-		dest[index] = '\0';
-
+		while (k < n && src[k] != '\0')
+		{
+			dest[k] = src[k];
+			k++;
+		}
+		while (k < n)
+		{
+			dest[k] = '\0';
+			k++;
+		}
 		return (dest);
 	}
 }
